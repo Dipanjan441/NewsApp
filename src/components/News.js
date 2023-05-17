f392469f4604d82bb5ddebb4295e79e5f51a3272
Dipanjan441 <dipanjantripathi441@gmail.com>
2023-05-17 21:43:17 +0530

    news,newsitem component created

--- src/components/News.js
+++ src/components/News.js
@@ -5,13 +5,31 @@ export class News extends Component {
   render() {
     return (
       <div>
-        This is News components
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+        <div className="container my-4">
+          <h2 className="news-heading">NewsApp-Here are your all latest and <strong>Top Headlines</strong></h2>
+        <div className="row mb-4">
+            <div className="col-md-4">
+              <NewsItem title={"mytitle"} description={"mydescription"}/>
+            </div>
+            <div className="col-md-4">
+              <NewsItem title={"mytitle"} description={"mydescription"}/>
+            </div>
+            <div className="col-md-4">
+              <NewsItem title={"mytitle"} description={"mydescription"}/>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-4">
+              <NewsItem title={"mytitle"} description={"mydescription"}/>
+            </div>
+            <div className="col-md-4">
+              <NewsItem title={"mytitle"} description={"mydescription"}/>
+            </div>
+            <div className="col-md-4">
+              <NewsItem title={"mytitle"} description={"mydescription"}/>
+            </div>
+        </div>
+        </div>
       </div>
     )
   }
